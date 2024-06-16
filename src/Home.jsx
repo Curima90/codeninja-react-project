048884b7dac86ae6948e5c51304a11b34a7a6617
Curima90 <curima.gm@gmail.com>
2024-06-16 01:30:34 +0100

    final commit of the night

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,24 +2,20 @@ import {useState} from 'react';
 
 const Home = () => {
 
-    //let name = 'mario';
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(25);
-
-
-    const handleClick = () => {
-        setName('luigi');
-        setAge(30);
-    };
-
-
-
+    const [posts, setPosts] = useState([
+        {title: 'This is the very first post', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', author: 'cazzabubbolo', id: '1'},
+        {title: 'This is the second post', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', author: 'bazzacuccolo', id: '2'},
+        {title: 'This is the third post', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', author: 'lazzabuccoco', id: '3'},
+    ])
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name } is { age } years old</p>
-            <button onClick={handleClick}>Click me</button>
+          {posts.map((posts) => (
+            <div className="post-preview" key={posts.id}>
+                <h2>{ posts.title} </h2>
+                <p> Written by {posts.author} </p>
+            </div>
+          ))}
         </div>
      );
 }
